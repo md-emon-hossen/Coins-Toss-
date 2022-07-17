16bdae17d50dbe62e786fb282e6fc887a4ee91d3
emon-hossen <mdemong87@gmail.com>
2022-07-17 21:31:36 +0600

    Initial Commited

---
+++ script.js
@@ -0,0 +1,60 @@
+var coin=document.querySelector(".img");
+var notify=document.querySelector(".notify");
+var showMessege=document.querySelector(".show");
+var flowerBtn=document.querySelector(".flowerBtn");
+var manBtn=document.querySelector(".manBtn");
+
+var coins=["img/1.jpg","img/2.jpg"];
+
+function toss(){
+
+    var randomNumber=Math.floor(Math.random()*2);
+    coin.src=coins[randomNumber];
+    return randomNumber;
+}
+
+
+
+
+flowerBtn.addEventListener("click",()=>{
+    mainToss(1);
+});
+manBtn.addEventListener("click",()=>{
+    mainToss(0);
+});
+
+
+function mainToss(vle){
+    var Value=vle;
+    var interValId=setInterval(toss,20);
+       setTimeout(()=>{
+        clearInterval(interValId);
+        var ranNum=toss();
+        if(Value == ranNum){
+            showMessege.textContent="Congratulation! You have won.";            
+        }else{
+            showMessege.textContent="Sorry! you have loss.";
+        }
+        
+       },3000);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
